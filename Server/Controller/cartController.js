46c990cbdc97acc7cfc1b08d09da4bf1069439cc
OmarAdel <omaradel1114@gmail.com>
2024-10-05 14:25:16 +0300

    Transformed the arhitecture to MVC , added the controller folder

---
+++ Server/Controller/cartController.js
@@ -0,0 +1,43 @@
+const Cart = require('../models/cartModel');
+
+const getCart =  async (req, res) => {
+    try {
+      const cart = await Cart.find({ user: req.params.userId })
+        .populate({
+          path: "cartProducts",
+          select: "prodTitle price brand imageUrl",
+        })
+        .populate({
+          path: "cartOwner",
+          select: "userName",
+        });
+  
+      res.status(200).json({ Status: "Success", data: { cart } });
+    } catch (error) {
+      console.error("cannot get the Cart details:", error);
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", message: error.message });
+    }
+  }
+const addToCart =  async (req, res) => {
+    try {
+      const { cartOwner, cartProducts } = req.body;
+  
+      const cart = new Cart({
+        cartOwner,
+        cartProducts,
+      });
+      const newCart = await cart.save();
+  
+      res.status(201).json({ Status: "Success", data: { newCart } });
+    } catch (error) {
+      console.error("Failed", error);
+      res.status(500).json({ error: "Failed", message: error.message });
+    }
+  }
+
+module.exports= {
+    getCart,
+    addToCart
+}
--- Server/routes/cartRouter.js
+++ Server/routes/cartRouter.js
@@ -1,47 +1,11 @@
 const express = require("express");
 const router = express.Router();
 router.use(express.json());
-const mongoose = require("mongoose");
-const Cart = require("../models/cartModel");
-
+const cartController = require('../Controller/cartController');
 const verifyToken = require("../middleware/auth.middleware");
 
-router.get("/:userId", verifyToken, async (req, res) => {
-  try {
-    const cart = await Cart.find({ user: req.params.userId })
-      .populate({
-        path: "cartProducts",
-        select: "prodTitle price brand imageUrl",
-      })
-      .populate({
-        path: "cartOwner",
-        select: "userName",
-      });
 
-    res.status(200).json({ Status: "Success", data: { cart } });
-  } catch (error) {
-    console.error("cannot get the Cart details:", error);
-    res
-      .status(500)
-      .json({ error: "Internal Server Error", message: error.message });
-  }
-});
-
-router.post("/add", verifyToken, async (req, res) => {
-  try {
-    const { cartOwner, cartProducts } = req.body;
-
-    const cart = new Cart({
-      cartOwner,
-      cartProducts,
-    });
-    const newCart = await cart.save();
-
-    res.status(201).json({ Status: "Success", data: { newCart } });
-  } catch (error) {
-    console.error("Failed", error);
-    res.status(500).json({ error: "Failed", message: error.message });
-  }
-});
+router.get("/:userId", verifyToken,cartController.getCart);
+router.post("/add", verifyToken,cartController.addToCart);
 
 module.exports = router;
